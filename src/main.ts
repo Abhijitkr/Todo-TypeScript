c3b6b7fa3bb7ad7da661fa06a3489cf1bd780ea2
AbhijitKr <abhijitkr600@gmail.com>
2024-03-21 19:34:26 +0530

    Adds the completed todo and delete todo functionality

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,64 @@
-import './style.css'
+import "./style.css";
 
-const form = document.querySelector('form') as HTMLFormElement;
-const todoField = document.getElementById('todoField') as HTMLInputElement;
+const form = document.querySelector("form") as HTMLFormElement;
+const todoField = document.getElementById("todoField") as HTMLInputElement;
 // const todoBtn = document.createElement('createTodo') as HTMLButtonElement;
-const todoContainer = document.getElementById('todoContainer') as HTMLDivElement;
+const todoContainer = document.getElementById(
+  "todoContainer"
+) as HTMLDivElement;
 
-interface todoType{
-  id: string,
-  title: string,
-  isCompleted: boolean
+interface todoType {
+  id: string;
+  title: string;
+  isCompleted: boolean;
 }
 
-
-form.onsubmit = (e:SubmitEvent) =>{
+const todoList: todoType[] = [];
+form.onsubmit = (e: SubmitEvent) => {
   e.preventDefault();
-  const todoList: todoType[] = [];
   const todo: todoType = {
-    id: String(Math.random()*1000),
+    id: String(Math.random() * 1000),
     title: todoField.value,
-    isCompleted: false
-  } 
-  todoField.value="";
+    isCompleted: false,
+  };
+  todoField.value = "";
   todoList.push(todo);
   renderTodoList(todoList);
-  console.log(todoList)
+  console.log(todoList);
+};
+
+const ul: HTMLUListElement = document.createElement("ul");
+ul.id = "todoList";
+
+function deleteTodo(id: string) {
+  const idx = todoList.findIndex((todo) => todo.id === id);
+  todoList.splice(idx, 1);
+  renderTodoList(todoList);
 }
 
-const ul: HTMLUListElement = document.createElement('ul');
-  ul.id = 'todoList';
-
-
-function generateTodoList(title:string, isCompleted:boolean) {
-  
-  const checkbox:HTMLInputElement = document.createElement('input');
-  checkbox.type = 'checkbox';
+function generateTodoList(id: string, title: string, isCompleted: boolean) {
+  const checkbox: HTMLInputElement = document.createElement("input");
+  checkbox.type = "checkbox";
   checkbox.checked = isCompleted;
-  checkbox.className = 'checkCompleted';
-  
-  const remove:HTMLButtonElement= document.createElement('button');
-  remove.textContent = 'X';
-  remove.className ='removeBtn';
-  // remove.setAttribute('onclick', deleteTodo);
+  checkbox.onchange = () => {
+    todoList.findIndex((todo) => {
+      if (todo.id === id) todo.isCompleted = checkbox.checked;
+    });
+    p.className = checkbox.checked ? "checkCompleted" : "";
+  };
 
-  const p:HTMLParagraphElement = document.createElement('p');
-  p.innerText=title;
-  
-  const li:HTMLLIElement= document.createElement('li');
-  li.className= 'todo';
+  const remove: HTMLButtonElement = document.createElement("button");
+  remove.textContent = "X";
+  remove.className = "removeBtn";
+  remove.onclick = () => deleteTodo(id);
+
+  const p: HTMLParagraphElement = document.createElement("p");
+  p.className = checkbox.checked ? "checkCompleted" : "";
+  p.innerText = title;
+
+  const li: HTMLLIElement = document.createElement("li");
+  li.className = "todo";
+  // li.id = id;
   li.append(checkbox, p, remove);
 
   ul.appendChild(li);
@@ -54,12 +66,8 @@ function generateTodoList(title:string, isCompleted:boolean) {
 }
 
 function renderTodoList(todoList: todoType[]) {
-  todoList.forEach(item=> generateTodoList(item.title, item.isCompleted));
+  ul.innerHTML = "";
+  todoList.forEach((item) =>
+    generateTodoList(item.id, item.title, item.isCompleted)
+  );
 }
-
-
-
-
-
-
-
